70646f2b948975dbf1c44b272c50c503cd08e750
Edielson <edielsonrodrigues567@gmail.com>
2022-08-08 12:42:49 -0300

    first commit

--- soma.py
+++
@@ -1,6 +0,0 @@
-num1= input("Digite o primeiro número: " )
-num2= input("Digite o segundo número: " )
-
-soma = (float(num1)+float(num2))
-
-print(soma)
--- teste02.py
+++
@@ -1,5 +0,0 @@
-num1 = input("numero1: ")
-num2 = input("numero2: ")
-
-soma=(float(num1) + float(num2))
-print(soma)
