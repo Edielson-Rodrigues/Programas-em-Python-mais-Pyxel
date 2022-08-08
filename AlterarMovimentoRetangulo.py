1e78ca2e9aa64cf1117ba0507ebf47f8111f2be7
Edielson <edielsonrodrigues567@gmail.com>
2022-08-08 13:55:03 -0300

    comentários para facilitar entedimento

--- AlterarMovimentoRetangulo.py
+++ AlterarMovimentoRetangulo.py
@@ -11,17 +11,18 @@ class Teste:
         pyxel.run(self.update, self.draw)
 
     def update(self):
-
+        #alterando movimento, direcionando para baixo(o eixo y cresce de cima para baixo)
             if self.condicao:
                 self.posicaoP1 += self.aceleracao 
                 if self.posicaoP1 + self.altura == pyxel.height:
                     self.condicao = False
 
+        #alterando movimento, direcionando para cima(o eixo y diminui de baixo para cima)
             else: 
                 self.posicaoP1 -= self.aceleracao
                 if self.posicaoP1 == 0:
                     self.condicao = True
-            
+        #obs: o eixo x, cresce da esquerda para direita e diminui da direita para esquerda
     def draw(self):
         pyxel.cls(1)
         pyxel.rect(2, self.posicaoP1, 3, self.altura, 3)
